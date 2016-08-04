38527da0b2b69f1816ef1ab804247bf2c1cabc97
Insan Ganang Putranda <insan.putranda@gmail.com>
2016-08-04 16:17:09 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Vagrant-Phalcon-3-PHP7-Xenial
+# Vagrant Phalcon 3 PHP7 Xenial
+
+This repository contains initial setup for compiling phalcon 3 in php7 under ubuntu xenial using vagrant.
+
+The sync folder is by default located at ../data/ with the docroot at ../data/html/.
+
+The bootstrap.sh contains some lines to fix missing attributes in xenial image. Currently, the vagrant setup needs to be run manually two times since it fails to mount properly.
+
+In between each vagrant up, virtual box utility needs to be installed for mounting :
+
+apt-get --no-install-recommends install -y virtualbox-guest-utils
---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo -s
+
+echo '127.0.0.1 ubuntu-xenial' >> /etc/hosts
+echo 'export LC_CTYPE=en.US.utf8' >> ~/.bashrc
+echo 'export LC_ALL=en_US.utf8' >> ~/.bashrc
+
+apt-get update
+apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+apt-get install -y php libapache2-mod-php php-mcrypt php-mysql
+apt-get --no-install-recommends install -y virtualbox-guest-utils
+
+apt-add-repository ppa:phalcon/stable
+apt-get update
+apt-get install -y php7.0-phalcon
+apt-get install -y php7.0-dev
+
+cd /home/ubuntu/
+
+git clone --depth=1 git://github.com/phalcon/cphalcon.git
+cd cphalcon/build
+sudo ./install
+
+# time to set phalcon extension from php7
+ln -s /etc/php/7.0/mods-available/phalcon.ini /etc/php/7.0/apache2/conf.d/50-phalcon.ini
+
+# enabled rewrite mod
+ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/rewrite.load
+sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf 
+
+# restart webservice
+service apache2 restart
